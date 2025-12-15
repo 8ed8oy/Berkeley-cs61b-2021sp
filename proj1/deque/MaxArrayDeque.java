29e67df7d5599efcf37cb6070aa678f236c86f65
8ed8oy <1787144656@qq.com>
2025-12-15 21:13:52 +0800

    MaxArrayDeque done.

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,19 +3,28 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private Comparator<T> comparator;
 
-    public MaxArrayDeque(Comparator<T> comparator) {
+    public MaxArrayDeque(Comparator<T> c) {
         super();
+        this.comparator = c;
     }
 
     public T max(){
-
+        return max(this.comparator);
     }
 
     public T max(Comparator<T> comparator){
         if (isEmpty()){
             return null;
         }
-
+        T maxItem = get(0);
+        for (int i = 1; i < size(); i++){
+            T currentItem = get(i);
+            if (comparator.compare(currentItem, maxItem) > 0){
+                maxItem = currentItem;
+            }
+        }
+        return maxItem;
     }
 }
